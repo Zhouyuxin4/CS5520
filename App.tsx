8ce6b0e8aa5ab77bdc08d25f3020b9282eeab326
Yuxin Zhou <zhou.yuxin4@northeastern.edu>
2025-01-13 17:09:58 -0800

    add InputProps

--- App.tsx
+++ App.tsx
@@ -1,16 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View, Text } from 'react-native';
 import Header from "./components/Header"
 import Input from "./components/Input"
+import { useState } from 'react';
 
 export default function App() {
+  const [isFocused, setIsFocused] = useState(true)
   const appName = "My awesome app"
-
 
   return (
     <View style={styles.container}>
-      <Header name = {appName}/>
-      <Input/>
+      <Header name={appName} />
+      <Input isFocused={isFocused} />
+      <Text 
+        onPress={() => setIsFocused(!isFocused)}
+      >
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
