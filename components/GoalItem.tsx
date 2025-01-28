6c6f159a53980f6f0f9b8b636d681352792fec4c
Yuxin Zhou <zhou.yuxin4@northeastern.edu>
2025-01-28 01:59:03 -0800

    modify styling

--- components/GoalItem.tsx
+++ components/GoalItem.tsx
@@ -13,47 +13,24 @@ interface GoalItemProps{
 export default function GoalItem({goalObj, deleteHandler}:GoalItemProps){
     return (
         <View style={styles.output}>
-          <Text>{goalObj.text}</Text>
+          <Text style={styles.text}>{goalObj.text}</Text>
           <Button title="X" onPress={()=>{deleteHandler(goalObj.id)}}/>
         </View>
       );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    welcome: {
-      borderColor: "purple",
-      borderWidth: 2,
-      borderRadius: 10,
-      padding: 10,
-    },
-    button: {
-      marginTop: 20,
-      width: "30%"
-    },
     output: {
-      marginTop: 100,
-      color: "purple",
+      marginBottom:100,
       backgroundColor: "#ccc",
       padding: 10,
       borderRadius: 10,
       flexDirection:"row",
       alignItems:"center"
     },
-    topContainer: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    buttonContainer: {
-      flex: 4,
-      alignItems: 'center',
-      backgroundColor: "pink",
+    text:{
+      color:"#800080",
+      fontSize:16,
     }
   });
   
